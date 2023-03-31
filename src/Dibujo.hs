0f7e1b060b1df57703aff0ac165beec071f69435
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-03-31 12:20:22 -0300

    comienzo

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -11,34 +11,51 @@ module Dibujo (
 
 
 {-
-Gramática de las figuras:
+--Gramática de las figuras:
 <Fig> ::= Figura <Bas> | Rotar <Fig> | Espejar <Fig> | Rot45 <Fig>
     | Apilar <Float> <Float> <Fig> <Fig> 
     | Juntar <Float> <Float> <Fig> <Fig> 
     | Encimar <Fig> <Fig>
 -}
 
-
-data Dibujo a = Borrar -- Completar
+data Dibujo a = Figura a | Rotar Dibujo a | Espejar Dibujo a 
+    | Rot45 Dibujo a
+    | Apilar Float Float Dibujo a Dibujo a 
+    | Juntar Float Float Dibujo a Dibujo a
+    | Encimar Dibujo a Dibujo a
     deriving (Eq, Show)
 
 -- Agreguen los tipos y definan estas funciones
 
 -- Construcción de dibujo. Abstraen los constructores.
 
-figura = undefined
+figura :: a -> Dibujo a
+figura = Figura
 
-rotar = undefined
+rotar :: a -> Dibujo a
+rotar = Rotar
 
-espejar = undefined
+espejar :: a -> Dibujo a
+espejar = Espejar
 
-rot45 = undefined
+rot45 :: a -> Dibujo a
+rot45 = Rot45
 
-apilar = undefined
+apilar :: Float  -> Float  -> Dibujo a -> Dibujo a
+apilar = Apilar
 
-juntar = undefined
+juntar :: Float -> Float -> Dibujo a -> Dibujo a 
+juntar = Juntar
 
-encimar = undefined
+encimar :: Dibujo a -> Dibujo a 
+encimar = Encimar
+
+-- Composición n-veces de una función con sí misma. Componer 0 veces
+-- es la función constante, componer 1 vez es aplicar la función 1 vez, etc.
+-- Componer negativamente es un error!
+comp :: (a -> a) -> Int -> a -> a
+comp f 0 a = a
+comp f x a 
 
 
 -- Rotaciones de múltiplos de 90.
