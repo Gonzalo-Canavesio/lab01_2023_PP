6b58f48c2c3e76c1766a95614891f60426ccaf45
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-04-04 20:37:16 -0300

    folDib y mapDib, al menos una idea

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -61,18 +61,17 @@ comp f n a | n > 0 =   f (comp f (n-1) a)
 
 -- Rotaciones de múltiplos de 90.
 r180 :: Dibujo a -> Dibujo a
-r180 a = comp rotar 2 a
+r180 a = comp rotar 2 
 
 r270 :: Dibujo a -> Dibujo a
-r270 a = comp rotar 3 a
+r270 a = comp rotar 3 
 
 -- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
 (.-.) :: Dibujo a -> Dibujo a -> Dibujo a
-(.-.) a b = apilar 0.5 0.5 a b  --nose sie hay que poer 0,5 o algo mas grande  
+(.-.) a b = apilar 0.5 0.5 a b --nose sie hay que poer 0,5 o algo mas grande  
 -- Pone una figura al lado de la otra, ambas ocupan el mismo espacio.
 (///) :: Dibujo a -> Dibujo a -> Dibujo a
-(///) a b = juntar 0.5 0.5 a b  
-
+(///) a b = juntar 0.5 0.5 a b
 -- Superpone una figura con otra.
 (^^^) :: Dibujo a -> Dibujo a -> Dibujo a
 (^^^) a b = encimar a b
@@ -90,7 +89,8 @@ encimar4 a = (^^^) a ((^^^) (rotar a) ((^^^) (r180 a) (r270 a)))
 
 -- Cuadrado con la misma figura rotada i * 90, para i ∈ {0, ..., 3}.
 -- No confundir con encimar4!
-ciclar = undefined
+ciclar :: Dibujo a -> Dibujo a
+ciclar a =  (.-.) ((///) a (r270 a)) ((///) (r180 a) (rotar a))
 
 -- Estructura general para la semántica (a no asustarse). Ayuda: 
 -- pensar en foldr y las definiciones de Floatro a la lógica
@@ -99,11 +99,24 @@ foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
        (Float -> Float -> b -> b -> b) -> 
        (b -> b -> b) ->
        Dibujo a -> b
-foldDib = undefined
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d = case d of
+    Figura a -> fFig a -- si d es una figura, aplico fFig a
+    Rotar d a -> fRot (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- si d es una rotacion, aplico foldDib a d para obtener el dibujo y aplico fRot a ese dibujo
+    Espejar d a -> fEsp (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
+    Rot45 d a -> fRot45 (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
+    Apilar x y d1 d2 a -> fApi x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una apilacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fApi a esos dibujos 
+    Juntar x y d1 d2 a -> fJun x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- lo mismo que apilar
+    Encimar d1 d2 a -> fEnc (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una encimacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fEnc a esos dibujos
 
 -- Demostrar que `mapDib figura = id`
 mapDib :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
-mapDib = undefined
+mapDib f (Figura a) = f a
+mapDib f (Rotar d a) = Rotar (mapDib f d) a
+mapDib f (Espejar d a) = Espejar (mapDib f d) a
+mapDib f (Rot45 d a) = Rot45 (mapDib f d) a
+mapDib f (Apilar x y d1 d2) = Apilar x y (mapDib f d1) (mapDib f d2)
+mapDib f (Juntar x y d1 d2) = Juntar x y (mapDib f d1) (mapDib f d2)
+mapDib f (Encimar d1 d2) = Encimar (mapDib f d1) (mapDib f d2)
 
 -- Junta todas las figuras básicas de un dibujo.
 figuras = undefined
