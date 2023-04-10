678ba58656dbde6d5fae00539718f6f7d5003690
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-04-09 22:55:52 -0300

    Arreglando errores

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -18,11 +18,11 @@ module Dibujo (
     | Encimar <Fig> <Fig>
 -}
 
-data Dibujo a = Figura a | Rotar Dibujo a | Espejar Dibujo a 
-    | Rot45 Dibujo a
-    | Apilar Float Float Dibujo a Dibujo a 
-    | Juntar Float Float Dibujo a Dibujo a
-    | Encimar Dibujo a Dibujo a
+data Dibujo a = Figura a | Rotar (Dibujo a) | Espejar (Dibujo a) 
+    | Rot45 (Dibujo a)
+    | Apilar Float Float (Dibujo a) (Dibujo a) 
+    | Juntar Float Float (Dibujo a) (Dibujo a)
+    | Encimar (Dibujo a) (Dibujo a)
     deriving (Eq, Show)
 
 -- Agreguen los tipos y definan estas funciones
@@ -32,23 +32,23 @@ data Dibujo a = Figura a | Rotar Dibujo a | Espejar Dibujo a
 figura :: a -> Dibujo a
 figura = Figura
 
-rotar :: a -> Dibujo a
-rotar = Rotar
+rotar :: Dibujo a -> Dibujo a
+rotar d = Rotar d
 
-espejar :: a -> Dibujo a
-espejar = Espejar
+espejar :: Dibujo a -> Dibujo a
+espejar d = Espejar d
 
-rot45 :: a -> Dibujo a
-rot45 = Rot45
+rot45 :: Dibujo a -> Dibujo a
+rot45 d = Rot45 d
 
-apilar :: Float  -> Float  -> Dibujo a -> Dibujo a
-apilar = Apilar
+apilar :: Float  -> Float  -> Dibujo a -> Dibujo a -> Dibujo a
+apilar x y d1 d2 = Apilar x y d1 d2
 
-juntar :: Float -> Float -> Dibujo a -> Dibujo a 
-juntar = Juntar
+juntar :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a 
+juntar x y d1 d2 = Juntar x y d1 d2 
 
-encimar :: Dibujo a -> Dibujo a 
-encimar = Encimar
+encimar :: Dibujo a -> Dibujo a -> Dibujo a
+encimar d1 d2 = Encimar d1 d2
 
 -- Composición n-veces de una función con sí misma. Componer 0 veces
 -- es la función constante, componer 1 vez es aplicar la función 1 vez, etc.
@@ -61,10 +61,10 @@ comp f n a | n > 0 =   f (comp f (n-1) a)
 
 -- Rotaciones de múltiplos de 90.
 r180 :: Dibujo a -> Dibujo a
-r180 a = comp rotar 2 
+r180 a = comp rotar 2 a
 
 r270 :: Dibujo a -> Dibujo a
-r270 a = comp rotar 3 
+r270 a = comp rotar 3 a
 
 -- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
 (.-.) :: Dibujo a -> Dibujo a -> Dibujo a
@@ -78,7 +78,7 @@ r270 a = comp rotar 3
 
 -- Dadas cuatro figuras las ubica en los cuatro cuadrantes.
 cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
-cuarteto a b c d = .-. (/// a b) (/// c d)  
+cuarteto a b c d = (.-.) ((///) a b) ((///) c d)  
 -- Junto a las figuras a y b en la parte de arriba donde a está a la izquierda y b a la derecha.
 -- Junto a las figuras c y d en la parte de abajo donde c está a la izquierda y d a la derecha.
 
@@ -101,19 +101,19 @@ foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
        Dibujo a -> b
 foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d = case d of
     Figura a -> fFig a -- si d es una figura, aplico fFig a
-    Rotar d a -> fRot (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- si d es una rotacion, aplico foldDib a d para obtener el dibujo y aplico fRot a ese dibujo
-    Espejar d a -> fEsp (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
-    Rot45 d a -> fRot45 (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
-    Apilar x y d1 d2 a -> fApi x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una apilacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fApi a esos dibujos 
-    Juntar x y d1 d2 a -> fJun x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- lo mismo que apilar
-    Encimar d1 d2 a -> fEnc (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una encimacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fEnc a esos dibujos
+    Rotar d -> fRot (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- si d es una rotacion, aplico foldDib a d para obtener el dibujo y aplico fRot a ese dibujo
+    Espejar d -> fEsp (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
+    Rot45 d -> fRot45 (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
+    Apilar x y d1 d2 -> fApi x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una apilacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fApi a esos dibujos 
+    Juntar x y d1 d2 -> fJun x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- lo mismo que apilar
+    Encimar d1 d2 -> fEnc (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una encimacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fEnc a esos dibujos
 
 -- Demostrar que `mapDib figura = id`
 mapDib :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
 mapDib f (Figura a) = f a
-mapDib f (Rotar d a) = Rotar (mapDib f d) a
-mapDib f (Espejar d a) = Espejar (mapDib f d) a
-mapDib f (Rot45 d a) = Rot45 (mapDib f d) a
+mapDib f (Rotar d) = Rotar (mapDib f d) 
+mapDib f (Espejar d) = Espejar (mapDib f d) 
+mapDib f (Rot45 d) = Rot45 (mapDib f d) 
 mapDib f (Apilar x y d1 d2) = Apilar x y (mapDib f d1) (mapDib f d2)
 mapDib f (Juntar x y d1 d2) = Juntar x y (mapDib f d1) (mapDib f d2)
 mapDib f (Encimar d1 d2) = Encimar (mapDib f d1) (mapDib f d2)
--- src/Pred.hs
+++ src/Pred.hs
@@ -3,14 +3,14 @@ module Pred (
   cambiar, anyDib, allDib, orP, andP
 ) where
 
-import Dibujo(Dibujo, foldDib, mapDib)
+import Dibujo(Dibujo, foldDib, mapDib, figura)
 
 type Pred a = a -> Bool
 
 -- Dado un predicado sobre básicas, cambiar todas las que satisfacen
 -- el predicado por la figura básica indicada por el segundo argumento.
 cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
-cambiar Pred p f dib = mapDib(\x -> if p x then f x else x) dib
+cambiar p f dib = mapDib(\x -> if p x then f x else figura x) dib
 -- Para esta funcion utilizamos la funcion mapdib donde utilizamos una funcion lambda
 -- que chequea si el predicado se cumple para la figura basica, si se cumple se aplica la funcion f en caso contrario se devuelve la figura basica
 
