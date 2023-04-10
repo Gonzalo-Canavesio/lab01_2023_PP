7b61026cdb9ea7e0745aa9ed6cab62e0fe32f50a
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2023-04-10 07:16:17 -0300

    Realizadas un par de correcciones de código y de formato

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -33,22 +33,22 @@ figura :: a -> Dibujo a
 figura = Figura
 
 rotar :: Dibujo a -> Dibujo a
-rotar d = Rotar d
+rotar = Rotar
 
 espejar :: Dibujo a -> Dibujo a
-espejar d = Espejar d
+espejar = Espejar
 
 rot45 :: Dibujo a -> Dibujo a
-rot45 d = Rot45 d
+rot45 = Rot45
 
 apilar :: Float  -> Float  -> Dibujo a -> Dibujo a -> Dibujo a
-apilar x y d1 d2 = Apilar x y d1 d2
+apilar = Apilar
 
 juntar :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a 
-juntar x y d1 d2 = Juntar x y d1 d2 
+juntar = Juntar
 
 encimar :: Dibujo a -> Dibujo a -> Dibujo a
-encimar d1 d2 = Encimar d1 d2
+encimar = Encimar
 
 -- Composición n-veces de una función con sí misma. Componer 0 veces
 -- es la función constante, componer 1 vez es aplicar la función 1 vez, etc.
@@ -61,27 +61,28 @@ comp f n a | n > 0 =   f (comp f (n-1) a)
 
 -- Rotaciones de múltiplos de 90.
 r180 :: Dibujo a -> Dibujo a
-r180 a = comp rotar 2 a
+r180 = comp rotar 2
 
 r270 :: Dibujo a -> Dibujo a
-r270 a = comp rotar 3 a
+r270 = comp rotar 3
 
 -- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
 (.-.) :: Dibujo a -> Dibujo a -> Dibujo a
-(.-.) a b = apilar 0.5 0.5 a b --nose sie hay que poer 0,5 o algo mas grande  
+(.-.) = apilar 1.0 1.0
+
 -- Pone una figura al lado de la otra, ambas ocupan el mismo espacio.
 (///) :: Dibujo a -> Dibujo a -> Dibujo a
-(///) a b = juntar 0.5 0.5 a b
+(///) = juntar 1.0 1.0
+
 -- Superpone una figura con otra.
 (^^^) :: Dibujo a -> Dibujo a -> Dibujo a
-(^^^) a b = encimar a b
+(^^^) = encimar
 
 -- Dadas cuatro figuras las ubica en los cuatro cuadrantes.
 cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
 cuarteto a b c d = (.-.) ((///) a b) ((///) c d)  
 -- Junto a las figuras a y b en la parte de arriba donde a está a la izquierda y b a la derecha.
 -- Junto a las figuras c y d en la parte de abajo donde c está a la izquierda y d a la derecha.
-
 
 -- Una figura repetida con las cuatro rotaciones, superpuestas.
 encimar4 :: Dibujo a -> Dibujo a 
@@ -90,7 +91,7 @@ encimar4 a = (^^^) a ((^^^) (rotar a) ((^^^) (r180 a) (r270 a)))
 -- Cuadrado con la misma figura rotada i * 90, para i ∈ {0, ..., 3}.
 -- No confundir con encimar4!
 ciclar :: Dibujo a -> Dibujo a
-ciclar a =  (.-.) ((///) a (r270 a)) ((///) (r180 a) (rotar a))
+ciclar a =  (.-.) ((///) a (rotar a)) ((///) (r180 a) (r270 a))
 
 -- Estructura general para la semántica (a no asustarse). Ayuda: 
 -- pensar en foldr y las definiciones de Floatro a la lógica
@@ -99,14 +100,14 @@ foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
        (Float -> Float -> b -> b -> b) -> 
        (b -> b -> b) ->
        Dibujo a -> b
-foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d = case d of
-    Figura a -> fFig a -- si d es una figura, aplico fFig a
-    Rotar d -> fRot (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- si d es una rotacion, aplico foldDib a d para obtener el dibujo y aplico fRot a ese dibujo
-    Espejar d -> fEsp (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
-    Rot45 d -> fRot45 (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
-    Apilar x y d1 d2 -> fApi x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una apilacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fApi a esos dibujos 
-    Juntar x y d1 d2 -> fJun x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- lo mismo que apilar
-    Encimar d1 d2 -> fEnc (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una encimacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fEnc a esos dibujos
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc (Figura d) = fFig d -- si d es una figura, aplico fFig a d
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc (Rotar d) = fRot (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d)  -- Si d es una rotacion, aplico foldDib a d para obtener el dibujo y aplico fRot a ese dibujo
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc (Espejar d) = fEsp (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc (Rot45 d) = fRot45 (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d) -- lo mismo que rotar
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc (Apilar x y d1 d2) = fApi x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una apilacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fApi a esos dibujos
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc (Juntar x y d1 d2) = fJun x y (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- lo mismo que apilar
+foldDib fFig fRot fEsp fRot45 fApi fJun fEnc (Encimar d1 d2) = fEnc (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d1) (foldDib fFig fRot fEsp fRot45 fApi fJun fEnc d2) -- si d es una encimacion, aplico foldDib a d1 y d2 para obtener los dibujos y aplico fEnc a esos dibujos
+
 
 -- Demostrar que `mapDib figura = id`
 mapDib :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
@@ -120,8 +121,8 @@ mapDib f (Encimar d1 d2) = Encimar (mapDib f d1) (mapDib f d2)
 
 -- Junta todas las figuras básicas de un dibujo.
 figuras :: Dibujo a -> [a]
-figuras a = foldDib (\a -> [a]) (id) (id) (id) (\x y a b -> a ++ b) (\x y a b -> a ++ b) (\a b -> a ++ b) a
--- (\a -> [a])  haciendo referencia a fFig de foldDib, que cada vez que encuentra una figura, la pone en una lista
--- (id) haciendo referencia a fRot, fEsp y fRot45 de foldDib, que cada vez que encuentra una rotacion, espejado o rotacion de 45, no hace nada
--- (\x y a b -> a ++ b) haciendo referencia a fApi y fJun de foldDib, que cada vez que encuentra una apilacion o una juntacion, concatena las listas de figuras de los dos dibujos
--- (\a b -> a ++ b) haciendo referencia a fEnc de foldDib, que cada vez que encuentra una encimacion, concatena las listas de figuras de los dos dibujos
+figuras = foldDib (:[]) id id id (\_ _ a b -> a ++ b) (\_ _ a b -> a ++ b) (++)
+-- (:[])  haciendo referencia a fFig de foldDib, que cada vez que encuentra una figura, la agrega a una lista con solo esa figura
+-- id haciendo referencia a fRot, fEsp y fRot45 de foldDib, que cada vez que encuentra una rotacion, espejado o rotacion de 45, no hace nada.
+-- (\_ _ a b -> a ++ b) haciendo referencia a fApi y fJun de foldDib, que cada vez que encuentra una apilacion o una juntacion, concatena las listas de figuras de los dos dibujos
+-- (++) haciendo referencia a fEnc de foldDib, que cada vez que encuentra una encimacion, concatena las listas de figuras de los dos dibujos
--- src/Pred.hs
+++ src/Pred.hs
@@ -10,22 +10,22 @@ type Pred a = a -> Bool
 -- Dado un predicado sobre básicas, cambiar todas las que satisfacen
 -- el predicado por la figura básica indicada por el segundo argumento.
 cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
-cambiar p f dib = mapDib(\x -> if p x then f x else figura x) dib
+cambiar p f = mapDib(\x -> if p x then f x else figura x)
 -- Para esta funcion utilizamos la funcion mapdib donde utilizamos una funcion lambda
 -- que chequea si el predicado se cumple para la figura basica, si se cumple se aplica la funcion f en caso contrario se devuelve la figura basica
 
 -- Alguna básica satisface el predicado.
 anyDib :: Pred a -> Dibujo a -> Bool
-anyDib p dib = foldDib p id id id (\a b x y -> x || y) (\a b x y -> x || y) (\x y -> x || y) dib 
+anyDib p = foldDib p id id id (\_ _ x y -> x || y) (\_ _ x y -> x || y) (||)
 
 -- Todas las básicas satisfacen el predicado.
 allDib :: Pred a -> Dibujo a -> Bool
-allDib p dib = foldDib p id id id (\a b x y -> x && y) (\a b x y -> x && y) (\x y -> x && y) dib
+allDib p = foldDib p id id id (\_ _ x y -> x && y) (\_ _ x y -> x && y) (&&)
 
 -- Los dos predicados se cumplen para el elemento recibido.
 andP :: Pred a -> Pred a -> Pred a
-andP p q = \x -> p x && q x
+andP p q x = p x && q x
 
 -- Algún predicado se cumple para el elemento recibido.
 orP :: Pred a -> Pred a -> Pred a
-orP p q = \x -> p x || q x
+orP p q x = p x || q x
